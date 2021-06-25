48697cd60f0eba07ecf3b54c25ecd09e63c82593
Christian Skorobogatow <christiansko@icloud.com>
2021-06-25 17:34:43 +0200

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Christian Skorobogatow on 25/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

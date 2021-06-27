5a7f74d83f4a54c4e4a1f2b865f5886d8845f9c8
Christian Skorobogatow <christiansko@icloud.com>
2021-06-27 12:29:43 +0200

    feat: added slider and segmented control to UI and value changed references

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nounTextfield: UITextField!
+    @IBOutlet weak var adjectiveTextfield: UITextField!
+    @IBOutlet weak var verbTextfield: UITextField!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    
+    let storyPrompt = StoryPromptEntry()
+    
+    @IBAction func changedNumber(_ sender: UISlider) {
+        
+        numberLabel.text = "Number \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func changedStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+        
+        print(storyPrompt.genre)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let storyPrompt = StoryPromptEntry()
+        
+        numberSlider.value = 7.5
         
         storyPrompt.noun = "toasts"
         storyPrompt.adjective = "smelly"
